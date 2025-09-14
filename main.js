c76714b16031c64f87c339efd63c311fdb8c7d5f
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-09-14 23:13:48 +0300

    commit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const moment = require('moment');
 
 
-let getDate = ()=> console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
 
-getDate();
+let getDate = ()=> console.log(moment().format('YYYY/MM/DD HH:mm:ss'));
+// getDate();
+
+let getCurrentWeekday = () => console.log(moment().format('dddd'))
+getCurrentWeekday()
