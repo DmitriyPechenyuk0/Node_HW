ceed576b508197e37d39bef227f8a123a4a7dd04
Dmitriy Pechenyuk <dmitriypechenyuk0@gmail.com>
2025-09-20 20:09:32 +0300

    add new endpoint (posts)

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const moment = require('moment');
-const express = require('express')
+const express = require('express');
+const fs = require('fs')
+const path = require('path')
+
+const pathToJson = path.join(__dirname, 'posts.json')
+
+const jsonFile = JSON.parse(fs.readFileSync(pathToJson, 'utf-8'))
 
 const app = express()
 
@@ -13,6 +19,10 @@ let getCurrentWeekday = () => moment().format('dddd');
 
 app.get("/timestamp", (req, res) =>{
     res.json(getDate())
+})
+
+app.get("/posts", (req, res) =>{
+    res.json(jsonFile)
 })
 
 app.listen(PORT, HOST, () => {
